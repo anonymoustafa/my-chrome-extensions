43fb7ea19f826934978d1158aaef810cadaf06d9
Mostafa Ramezani <anonymoustafa@outlook.com>
2024-02-09 11:48:47 -0500

    let's go to branch 003!

--- the-yanker!/background.js
+++ the-yanker!/background.js
@@ -1,3 +1,25 @@
 function logging(){
     console.log("log!");
 }
+
+document.getElementById('mySlider').addEventListener('input', function(event) {
+    console.log('Slider value:', event.target.value);
+  });
+
+
+
+function onOffLable(){
+    var slider = document.getElementById('mySlider');
+    var label = document.getElementById('mySliderLabel');
+    
+    switch(slider.value) {
+      case '0':
+        label.innerHTML = 'Off';
+        break;
+      case '1':
+        label.innerHTML = 'On';
+        break;
+    }
+}
+
+chrome.tabs.onUpdated.addL
